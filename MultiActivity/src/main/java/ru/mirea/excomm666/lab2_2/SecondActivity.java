2159133b52bd7654f9b510e4338556cc9032f464
Excomm666 <smolliklucky@gmail.com>
2019-12-04 13:02:29 +0300

    Initial commit

---
+++ MultiActivity/src/main/java/ru/mirea/excomm666/lab2_2/SecondActivity.java
@@ -0,0 +1,20 @@
+package ru.mirea.excomm666.lab2_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    TextView tv2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        tv2=findViewById(R.id.tv2);
+        Intent intent = getIntent();
+        String text =intent.getStringExtra("key");
+        tv2.setText(text);
+    }
+}
